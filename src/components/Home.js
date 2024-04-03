e4a408c72869afcb059e2897ec786a2696400716
Margarita Ramirez <margarita26.mr@gmail.com>
2024-04-03 10:48:35 -0400

    pass

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+import { name, city } from "../data/data";
+
+const divStyle = {
+  color: "firebrick"
+};
+
+const h1Info = `${name} is a Web Developer from ${city} `;
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (<div id="home">
+      <h1 style={divStyle}>{h1Info}</h1>
+  </div>
+  );
 }
 
 export default Home;
